4d87ff0e88491eaff8609d7133fa0b91a2d53eb8
Sonic-Haliza <74656879+Sonic-Haliza@users.noreply.github.com>
2022-07-09 13:47:35 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-            .✵.𝗚𝗥𝗨𝗣 𝗣𝗘𝗡𝗧𝗔𝗕𝗢𝗧.✵.
+            .Dana/Pulsa.
 
-            https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2
+          082211991425  
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
